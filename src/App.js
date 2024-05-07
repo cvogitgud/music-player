b5066ef8d98303bbf6480e4fcff3454bfa599038
Chris <cvo13014@gmail.com>
2024-05-07 15:08:04 -0700

    added some buttons via MUI for ui functionality, and a slider. need to get the slider to remain in the same width, but can't get it to work rn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import thegardenofwords from './img/thegardenofwords.jpg';
 import './App.css';
-import Button from '@mui/material/Button';
+import { Button, Slider, Stack } from '@mui/material/';
+
+import {
+  VolumeDown,
+  VolumeUp,
+  SkipNext,
+  SkipPrevious,
+} from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
-      <div className="title text-6xl mt-10">Walrus Music</div>
-      <div className="SongDetails flex justify-center">
-        <img
-          src={thegardenofwords}
-          alt="garden of words"
-          className="song-cover size-96 object-cover"
-        />
-      </div>
-      <div className="SongInteract">
-        <Button className="play-button">Play</Button>
-        <Button className="pause-button">Pause</Button>
+      <div className="content">
+        <div className="title text-6xl mt-10">Walrus Music</div>
+        <div className="SongDetails flex justify-center">
+          <div>
+            <img
+              src={thegardenofwords}
+              alt="garden of words"
+              className="song-cover size-96 object-cover"
+            />
+          </div>
+        </div>
+
+        <div className="tools ">
+          <div className="slider-ui">
+            <Stack
+              spacing={2}
+              direction="row"
+              sx={{ mb: 1 }}
+              alignItems="center">
+              <VolumeDown />
+              <Slider aria-label="Volume" />
+              <VolumeUp />
+            </Stack>
+            <div>
+              <Button>
+                <SkipPrevious />
+              </Button>
+              <Button className="play-button">Play</Button>
+              <Button className="pause-button">Pause</Button>
+              <Button>
+                <SkipNext />
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
