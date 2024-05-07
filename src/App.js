8540138f1117a8809b4cbcf8d9cf47b41c13c712
Chris <cvo13014@gmail.com>
2024-05-07 14:38:47 -0700

    started the layout, and added Material UI library for components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
+import thegardenofwords from './img/thegardenofwords.jpg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
-  return <div className="text-red-500 text-6xl">Text</div>;
+  return (
+    <div className="App">
+      <div className="title text-6xl mt-10">Walrus Music</div>
+      <div className="SongDetails flex justify-center">
+        <img
+          src={thegardenofwords}
+          alt="garden of words"
+          className="song-cover size-96 object-cover"
+        />
+      </div>
+      <div className="SongInteract">
+        <Button className="play-button">Play</Button>
+        <Button className="pause-button">Pause</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
