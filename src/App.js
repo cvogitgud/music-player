b927ebec237ee35fd42a1d583240ad8419fad177
Chris <cvo13014@gmail.com>
2024-05-07 15:40:26 -0700

    changed some margins, nothing big

--- src/App.js
+++ src/App.js
@@ -15,23 +15,21 @@ function App() {
       <div className="content">
         <div className="title text-6xl mt-10">Walrus Music</div>
         <div className="sections flex">
-          <div className="basis-1/3">
-            {/*empty filler, or maybe page links*/}
-          </div>
-          <div className="SongDetails flex justify-center mt-7 basis-1/3 ">
+          <div className="info mt-5 basis-1/3">Sidebar? Other pages?</div>
+          <div className="SongDetails flex justify-center mt-7 basis-1/3 align-top">
             <div>
               <img
                 src={thegardenofwords}
                 alt="garden of words"
-                className="song-cover size-96 object-cover"
+                className="track-art size-96 object-cover"
               />
 
-              <p className="m-3 ml-0 flex justify-start">
+              <p className="track-author-title m-3 ml-0 flex justify-start">
                 saib. - in your arms
               </p>
             </div>
           </div>
-          <div className="queue basis-1/3">Current queue</div>
+          <div className="queue basis-1/3 mt-5">Current queue</div>
         </div>
 
         <div className="tools ">
