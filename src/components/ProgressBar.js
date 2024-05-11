107177cfaed1a18b3cba5e7ab45fc85a6551fe25
Chris <cvo13014@gmail.com>
2024-05-10 17:15:57 -0700

    added formatting, sweet

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,6 +5,17 @@ const ProgressBar = ({ audioRef, isPlaying }) => {
   const [progress, setProgress] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
 
+  const formatTime = (time) => {
+    if (time && !isNaN(time)) {
+      const minutes = Math.floor(time / 60);
+      const formatMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+      const seconds = Math.floor(time % 60);
+      const formatSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+      return `${formatMinutes}:${formatSeconds}`;
+    }
+    return '00:00';
+  };
+
   useEffect(() => {
     setInterval(() => {
       setCurrentTime(audioRef.current.currentTime);
@@ -22,8 +33,8 @@ const ProgressBar = ({ audioRef, isPlaying }) => {
         className="mt-3 mb-3 w-96 flex justify-center flex-col"
       />
       <div className="time-display flex justify-between">
-        <p>{currentTime ? currentTime : '-:-'}</p>
-        <p>{audioRef.current.duration ? audioRef.current.duration : '-:-'}</p>
+        <p>{formatTime(currentTime)}</p>
+        <p>{formatTime(audioRef.current.duration)}</p>
       </div>
     </div>
   );
