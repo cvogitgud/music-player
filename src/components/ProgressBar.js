18e4fb8b5fd149e31b33cc74585859f352d2a60f
Chris <cvo13014@gmail.com>
2024-05-10 17:11:25 -0700

    gave up on figuring out the discrepancy between duration as state value and as raw audioRef property, but got to displaying the times correctly, formatting next

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,32 +1,30 @@
 import { useEffect, useState } from 'react';
 import { LinearProgress } from '@mui/material/';
 
-const ProgressBar = ({ audioRef }) => {
+const ProgressBar = ({ audioRef, isPlaying }) => {
   const [progress, setProgress] = useState(0);
   const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(audioRef.current.duration);
 
   useEffect(() => {
-    const timer = setInterval(() => {
+    setInterval(() => {
       setCurrentTime(audioRef.current.currentTime);
       setProgress(
         (audioRef.current.currentTime / audioRef.current.duration) * 100
       );
     }, 1000);
-
-    // if (!isPlaying) {
-    //   clearInterval(timer);
-    // }
   }, [audioRef.current.currentTime]);
 
   return (
     <div>
-      <p>{currentTime ? currentTime : 0}</p>
       <LinearProgress
         variant="determinate"
         value={progress}
         className="mt-3 mb-3 w-96 flex justify-center flex-col"
       />
+      <div className="time-display flex justify-between">
+        <p>{currentTime ? currentTime : '-:-'}</p>
+        <p>{audioRef.current.duration ? audioRef.current.duration : '-:-'}</p>
+      </div>
     </div>
   );
 };
--- src/data/tracks.js
+++ src/data/tracks.js
@@ -6,19 +6,19 @@ import inyourarms from './tracks/saib. - in your arms..mp3';
 import raw from './tracks/Victoria Justice - RAW.mp3';
 
 import thegardenofwords from './img/thegardenofwords.jpg';
-import raw_thumbnail from './img/Raw.webp';
+import raw_thumbnail from './img/Raw.png';
 
 export const tracks = [
-  {
-    title: 'in your arms.',
-    src: inyourarms,
-    thumbnail: thegardenofwords,
-    artist: 'saib',
-  },
   {
     title: 'RAW',
     src: raw,
     thumbnail: raw_thumbnail,
     artist: 'Victoria Justice',
+  },
+  {
+    title: 'in your arms.',
+    src: inyourarms,
+    thumbnail: thegardenofwords,
+    artist: 'saib',
   },
 ];
